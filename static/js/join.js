e9ac7f1e6c521c8b91949183ed84e7bb2c0508b4
aodtmdals <aodtmdals12@naver.com>
2022-09-22 09:22:11 +0900

    add kakao API

--- static/js/join.js
+++ static/js/join.js
@@ -1,3 +1,5 @@
+
+
 function sign_up() {
   let username = $("#input-username").val();
   let nickname = $("#input-nickname").val();
@@ -108,6 +110,77 @@ function sign_up() {
     },
     success: function (response) {
       alert("회원가입을 축하드립니다!");
+      window.location.replace("/");
+    },
+  });
+}
+
+function sign_revise() {
+  let nickname = $("#input-nickname").val();
+  let password = $("#input-password").val();
+  let password2 = $("#input-password2").val();
+  let phone = $("#input-phone").val();
+
+   if ($("#help-password").hasClass("is-danger")) {
+    alert("비밀번호를 다시 확인해주세요.");
+    return;
+  } else if ($("#help-nickname").hasClass("is-danger")) {
+    alert("닉네임을 다시 확인해주세요.");
+    return;
+  }
+
+  if (password == "") {
+    $("#help-password")
+      .text("비밀번호를 입력해주세요.")
+      .removeClass("is-safe")
+      .addClass("is-danger");
+    $("#input-password").focus();
+    return;
+  } else if (!is_password(password)) {
+    $("#help-password")
+      .text(
+        "비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자"
+      )
+      .removeClass("is-safe")
+      .addClass("is-danger");
+    $("#input-password").focus();
+    return;
+  } else {
+    $("#help-password")
+      .text("사용할 수 있는 비밀번호입니다.")
+      .removeClass("is-danger")
+      .addClass("is-success");
+  }
+  if (password2 == "") {
+    $("#help-password2")
+      .text("비밀번호를 입력해주세요.")
+      .removeClass("is-safe")
+      .addClass("is-danger");
+    $("#input-password2").focus();
+    return;
+  } else if (password2 != password) {
+    $("#help-password2")
+      .text("비밀번호가 일치하지 않습니다.")
+      .removeClass("is-safe")
+      .addClass("is-danger");
+    $("#input-password2").focus();
+    return;
+  } else {
+    $("#help-password2")
+      .text("비밀번호가 일치합니다.")
+      .removeClass("is-danger")
+      .addClass("is-success");
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "/user/revise/request",
+    data: {
+      pw: password,
+      nickname: nickname,
+    },
+    success: function (response) {
+      alert("회원정보 수정 완료!");
       window.location.replace("/");
     },
   });
